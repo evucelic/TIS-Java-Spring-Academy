599d854b3da961d337405c5555069a0922d668f6
Matija Jakovac <mj53871@fer.hr>
2024-08-28 14:14:37 +0200

    4 cijeli

--- product-manager/src/main/java/hr/tis/akademija/file/ProductWriter.java
+++ product-manager/src/main/java/hr/tis/akademija/file/ProductWriter.java
@@ -8,6 +8,8 @@ import java.io.File;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,21 +18,24 @@ import static hr.tis.akademija.file.FileSystemConfiguration.PRODUCTS_FILES_FOLDE
 
 public class ProductWriter {
 
-    public static void writeProducts(ProductsMetadata productsMetadata){
+    public static void writeProducts(ProductsMetadata productsMetadata) throws IOException {
         String fileName = String.format("%s_%s_%s.txt", productsMetadata.getId(),
                 productsMetadata.getDatumVrijemeKreiranja(), productsMetadata.getNaslov()).replace(":", "$");
+        File theDir = new File(PRODUCTS_FILES_FOLDER_PATH.resolve(fileName).toAbsolutePath().getParent().toString());
+        if (!theDir.exists()){
+            theDir.mkdirs();
+        }
         try (BufferedWriter writer = Files.newBufferedWriter(
                 PRODUCTS_FILES_FOLDER_PATH.resolve(fileName))) {
-            /* TODO pisanje proizvoda u datoteku
+            // TODO pisanje proizvoda u datoteku
             for (Product product : productsMetadata.getPopisProizvoda()) {
-                String string = String.format("%s%d%s%d", padRight(product.getNaziv(), 100),
+                String string = String.format("%s%s%s%d", padRight(product.getNaziv(), 100),
                         padLeft(product.getCijena().toString(), 10),
                         padRight(product.getMjernaJedinica(), 10),
                         product.getOcjena()
                 );
-                System.out.println(string);
-                writer.write(string);
-            }*/
+                writer.write(string + "\n");
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -45,7 +50,7 @@ public class ProductWriter {
         return String.format("%" + n + "s", s);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         List<Product> lista = new ArrayList<>();
         Product proizvod1 = new Product("mlijeko", new BigDecimal("5"), "EUR");
         Product proizvod2 = new Product("sir", new BigDecimal("10"), "EUR", 5);
