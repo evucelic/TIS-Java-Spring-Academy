5d28cb81cdc0fefeafc813cfdf16ef010f6f427f
vucel <vuceliceugen@gmail.com>
2024-09-03 09:57:22 +0200

    4.1 e) rjesen

--- product-manager/src/main/java/hr/tis/academy/App.java
+++ product-manager/src/main/java/hr/tis/academy/App.java
@@ -1,6 +1,8 @@
 package hr.tis.academy;
 
 import hr.tis.academy.repository.ProductRepository;
+import hr.tis.academy.repository.ProductRepositoryDB;
+import hr.tis.academy.repository.ProductRepositoryFile;
 import hr.tis.academy.repository.ProductRepositoryInMemory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
@@ -9,7 +11,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class App {
@@ -20,6 +24,9 @@ public class App {
     @Autowired
     private ProductRepositoryInMemory productRepositoryInMemory;
 
+    @Autowired
+    private ProductRepository productRepository;
+
 
     @Bean("myApplicationRunner")
     public ApplicationRunner applicationRunner() {
@@ -27,6 +34,12 @@ public class App {
             Arrays.stream(applicationContext.getBeanDefinitionNames()).forEach(System.out::println);
 
             System.out.printf("Products " + "record count: {" + productRepositoryInMemory.fetchProductsMetadataCount() + "}%n");
+
+            if (productRepository instanceof ProductRepositoryInMemory || productRepository instanceof ProductRepositoryFile || productRepository instanceof ProductRepositoryDB) {
+                System.out.println("instanca klase");
+            }
+
+            
             //org.springframework.boot.autoconfigure.internalCachingMetadataReaderFactory
         };
     }
