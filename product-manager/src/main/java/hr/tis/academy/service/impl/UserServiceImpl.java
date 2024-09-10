290c38455dc2d74d816286ebeccf6dc86c682ebf
vucel <vuceliceugen@gmail.com>
2024-09-10 14:09:52 +0200

    UserServiceImpl

--- product-manager/src/main/java/hr/tis/academy/service/impl/UserServiceImpl.java
+++ product-manager/src/main/java/hr/tis/academy/service/impl/UserServiceImpl.java
@@ -1,17 +1,28 @@
 package hr.tis.academy.service.impl;
 
-import hr.tis.academy.model.User;
+import hr.tis.academy.dto.UserDto;
+import hr.tis.academy.mapper.UserMapper;
 import hr.tis.academy.repository.UserRepository;
+import hr.tis.academy.repository.exception.UserAlreadyExistsException;
 import hr.tis.academy.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    private final UserMapper userMapper;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
         this.userRepository = userRepository;
+        this.userMapper = userMapper;
     }
 
     @Override
@@ -22,5 +33,12 @@ public class UserServiceImpl implements UserService {
             throw new UserAlreadyExistsException("Neispravan email");
         }
         userRepository.save(userMapper.toUserEntity(userDto));
+    }
+
+    private boolean validateEmail(String email) {
+        String regex = "^[A-Za-z0-9+_.-]+@(.+)$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
     }
 }
